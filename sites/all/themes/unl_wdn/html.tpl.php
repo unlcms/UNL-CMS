fc8d8b840bbe30c14de6ae1b905fc237caa9d00d
Tim Steiner <tsteiner2@unl.edu>
2010-06-30 17:23:39 +0000

    Move the UNL CAS logout link modification code from a javascript call to the <link rel="logout"> tag. Also add the return url to cas logouts.
    git-svn-id: file:///tmp/wdn_thm_drupal/branches/drupal-7.x@117 20a16fea-79d4-4915-8869-1ea9d5ebf173

--- sites/all/themes/unl_wdn/html.tpl.php
+++ sites/all/themes/unl_wdn/html.tpl.php
@@ -39,6 +39,7 @@ $t->head .= PHP_EOL
           . $styles . PHP_EOL
           . $scripts . PHP_EOL
           . '<link href="' . url('<front>', array('absolute' => TRUE)) . '" rel="home" />' . PHP_EOL
+          . '<link rel="logout" href="user/logout" title="Log out of myapp" />' . PHP_EOL
           ;
 
 if (theme_get_setting('use_base')) {
