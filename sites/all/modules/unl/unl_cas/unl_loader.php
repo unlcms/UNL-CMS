857ab134c6bce8f2a3c5ddbf7a8d42d3240bd137
Eric Rasmussen <eric@unl.edu>
2011-05-09 19:43:26 +0000

    [gh-123] Merging testing into staging (org unl dir)
    git-svn-id: file:///tmp/wdn_thm_drupal/branches/drupal-7.x/staging@626 20a16fea-79d4-4915-8869-1ea9d5ebf173

--- sites/all/modules/unl/unl_cas/unl_loader.php
+++ sites/all/modules/unl/unl_cas/unl_loader.php
@@ -2,12 +2,12 @@
 
 function unl_load_zend_framework() {
   static $isLoaded = FALSE;
-  
+
   if ($isLoaded) {
     return;
   }
-  
-  set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/../../libraries');
+
+  set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/../../../libraries');
   require_once 'Zend/Loader/Autoloader.php';
   $autoloader = Zend_Loader_Autoloader::getInstance();
   $autoloader->registerNamespace('Unl_');
