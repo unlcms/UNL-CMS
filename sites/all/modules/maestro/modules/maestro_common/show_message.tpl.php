66cead0a6b4fceba4ae4b1b515b39046241fb0ff
Eric Rasmussen <eric@unl.edu>
2011-07-18 22:31:45 +0000

    [gh-163] Merging testing to staging -c860
    git-svn-id: file:///tmp/wdn_thm_drupal/branches/drupal-7.x/staging@861 20a16fea-79d4-4915-8869-1ea9d5ebf173

--- sites/all/modules/maestro/modules/maestro_common/show_message.tpl.php
+++
@@ -1,6 +0,0 @@
-<div style="text-align:center;margin:5px;padding:10px;border:1px solid #CCC;font-size:14pt;"><?php print $message; ?></div>
-<div style="float:right;padding-right;">
-  <form style="margin:0px;">
-    <div style="font-size:10pt;"><input maestro="complete" type="button" value="<?php print t('Complete Task'); ?>"></div>
-  </form>
-</div>
