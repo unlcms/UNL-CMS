43b0bf92fabdf506b59becea45ebda3048405089
Eric Rasmussen <eric@unl.edu>
2011-12-19 23:13:21 +0000

    [gh-276] Merging test into staging -c1372
    git-svn-id: file:///tmp/wdn_thm_drupal/branches/drupal-7.x/staging@1373 20a16fea-79d4-4915-8869-1ea9d5ebf173

--- sites/all/themes/unl_wdn/theme-settings.php
+++ sites/all/themes/unl_wdn/theme-settings.php
@@ -41,34 +41,30 @@ function unl_wdn_form_system_theme_settings_alter(&$form, &$form_state) {
     ),
   );
 
-  $form[] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Site Name Abbreviation'),
-    'site_name_abbreviation' => array(
-      '#type' => 'textfield',
-      '#default_value' => theme_get_setting('site_name_abbreviation'),
-      '#description' => t('An abbreviated version of your site\'s name to use in breadcrumbs.'),
-    ),
-  );
-
   $form['intermediate_breadcrumbs'] = array(
     '#type' => 'fieldset',
     '#title' => t('Intermediate Breadcrumbs'),
     '#description' => t('Breadcrumbs that are displayed between the UNL breadcrumb and this site\'s breadcrumb'),
+    'site_name_abbreviation' => array(
+      '#type' => 'textfield',
+      '#title' => t('Site Name Abbreviation'),
+      '#default_value' => theme_get_setting('site_name_abbreviation'),
+      '#description' => t('An abbreviated version of your site\'s name to use in breadcrumbs when not on the front page.'),
+      '#weight' => 10,
+    ),
   );
-
   $intermediate_breadcrumbs = theme_get_setting('intermediate_breadcrumbs');
   for ($i = 0; $i < 3; $i++) {
     $form['intermediate_breadcrumbs'][] = array(
       'text' => array(
         '#type' => 'textfield',
-        '#title' => t('Text ' . ($i + 1)),
+        '#field_prefix' => t('Text ' . ($i + 1)),
         '#default_value' => isset($intermediate_breadcrumbs[$i]) ? $intermediate_breadcrumbs[$i]['text'] : '',
         '#parents' => array('intermediate_breadcrumbs' , $i, 'text'),
       ),
       'href' => array(
         '#type' => 'textfield',
-        '#title' => t('URL ' . ($i + 1)),
+        '#field_prefix' => t('&nbsp;URL ' . ($i + 1)),
         '#default_value' => isset($intermediate_breadcrumbs[$i]) ? $intermediate_breadcrumbs[$i]['href'] : '',
         '#parents' => array('intermediate_breadcrumbs' , $i, 'href'),
       ),
