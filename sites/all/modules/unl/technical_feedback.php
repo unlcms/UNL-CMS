9ee9c49e35acaf05b57a9358712be6ec637ea7ba
Eric Rasmussen <eric@unl.edu>
2012-04-06 22:14:40 +0000

    [gh-312] Merging test into staging -c1538
    git-svn-id: file:///tmp/wdn_thm_drupal/branches/drupal-7.x/staging@1539 20a16fea-79d4-4915-8869-1ea9d5ebf173

--- sites/all/modules/unl/technical_feedback.php
+++ sites/all/modules/unl/technical_feedback.php
@@ -1,116 +1,82 @@
 <?php
 
-function technical_feedback(){
-	echo drupal_render(drupal_get_form('technical_feedback_form'));
+function unl_technical_feedback($form, &$form_state) {
+  $form['root'] = array(
+    '#title' => 'UNLcms Technical Feeedback Form',
+  );
+
+   $form['root']['cas_username'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Username'),
+    '#value' => ($GLOBALS['user']->name),
+    '#disabled' => 'disabled',
+  );
+
+  $form['root']['email'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Email'),
+    '#value' => ($GLOBALS['user']->mail),
+    '#disabled' => 'disabled',
+  );
+
+  $form['root']['browser_useragent'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Browser UserAgent'),
+    '#value' => $_SERVER['HTTP_USER_AGENT'],
+    '#disabled' => 'disabled',
+    '#size' => 120,
+  );
+
+  $form['root']['site'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Site'),
+    '#value' => $GLOBALS['base_root'] . (isset($GLOBALS['base_path']) ? $GLOBALS['base_path'] : ''),
+    '#disabled' => 'disabled',
+    '#size' => 120,
+  );
+
+  $form['root']['current_url'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Page address in question'),
+    '#value' => $_SERVER['HTTP_REFERER'],
+    '#size' => 120,
+  );
+
+  $form['root']['technical_feedback'] = array(
+    '#type' => 'textarea',
+    '#title' => t('Please give your feedback or describe the issue you are having'),
+    '#required' => TRUE,
+  );
+
+  $form['root']['submit'] = array(
+    '#type' => 'submit',
+    '#value' => t('Submit'),
+  );
+
+  return $form;
 }
 
-function technical_feedback_form($form, &$form_state) {
+function unl_technical_feedback_submit($form, &$form_state) {
+  $to = "unlcms-dev@listserv.unl.edu";
+  $from = $form_state['values']['email'];
+  $subject = "UNLcms technical feedback from " . $form_state['values']['cas_username'];
 
-	 $form['root'] = array(
-      	'#type' => 'fieldset',
-      	'#title' => 'Technical Feeedback Form',
-  	);
-  	
-  	$form['root']['browser_useragent'] = array(
-  		'#type' => 'textfield',
-  		'#title' => t('Browser UserAgent (textfield disabled)'),
-  		'#value' => $_SERVER['HTTP_USER_AGENT'],
-  		'#disabled' => 'disabled',
-  	);
-  	
-  	$form['root']['current_url'] = array(
-  		'#type' => 'textfield',
-  		'#title' => t('Current url (text disabled)'),
-  		'#value' => $_SERVER['HTTP_REFERER'],
-  		'#disabled' => 'disabled'
-  	);
-  	
-  	 $form['root']['cas_username'] = array(
-        '#type' => 'textfield',
-        '#title' => t('CAS Name (textfield disabled'),
-        '#value' => ($GLOBALS['user']->name),
-        '#disabled' => 'disabled',
-       
-  	);
-  	
-  	$form['root']['email'] = array(
-  		'#type' => 'textfield',
-  		'#title' => t('Email (textfield disabled)'),
-  		'#value' => ($GLOBALS['user']->mail),
-  		'#disabled' => 'disabled'
-  
-  	);
-  	
-  	$form['root']['full_name'] = array(
-  		'#type' => 'textfield',
-  		'#title' => t('Your first, last name'),
-  		'#required' => TRUE,
-  	);
-  	
-  	$form['root']['technical_feedback'] = array(
-  		'#type' => 'textarea',
-  		'#title' => t('Please give your feedback or describe the issue you are having'),
-  		'#required' => TRUE,
-  	);
-  	
-  	$form['root']['submit'] = array(
-  		'#type' => 'submit',
-  		'#value' => t('Submit'),
-  	);
-  	    	
-  	
-  	return $form;    	
-}
+  $message = '
+Username: '.$form_state['values']['cas_username'].'
+Email: '.$form_state['values']['email'].'
+UserAgent: '.$form_state['values']['browser_useragent'].'
+Site: '.$form_state['values']['site'].'
+Page: '.$form_state['input']['current_url'].'
+Comment:
+'.$form_state['values']['technical_feedback'];
 
-function technical_feedback_form_submit($form, &$form_state){
-	$browser_useragent 		= $form_state['values']['browser_useragent'];
-	$current_url					= $form_state['values']['current_url'];
-	$cas_username					= $form_state['values']['cas_username'];
-	$email								= $form_state['values']['email'];
-	$full_name						= $form_state['values']['full_name'];
-	$technical_feedback		= $form_state['values']['technical_feedback'];
-	
-	$to = "unlcms-dev@listserv.unl.edu";
-	$from = $email;
-	$subject = "UNLcms Technical Feedback";
-	
-	$message = '
-	<html>
-		<body>
-			<table style="border:1px solid #bbb;cellpadding="10";">
-				<tr style="background-color: #eee;">
-					<td>Browser UserAgent</td><td>'.$browser_useragent.'</td>
-				</tr>
-				<tr>
-					<td>CAS Username</td><td>'.$cas_username.'</td>
-				</tr>
-				<tr style="background-color: #eee;">
-					<td>Full Name</td><td>'.$full_name.'</td>
-				</tr>
-				<tr>
-					<td>Email</td><td>'.$email.'</td>
-				</tr>
-				<tr style="background-color: #eee;">
-					<td>Page URL</td><td>'.$current_url.'</td>
-				</tr>
-				<tr>				
-					<td>Technical Feedback/Issue</td><td>'.$technical_feedback.'</td>
-				</tr>
-			</table>
-		</body>
-	</html>
-	';
-	
-	$technical_feedback_email_headers 	= 'From: ' . $email . "\r\n";
+  $headers   = 'From: ' . $form_state['values']['email'] . "\n";
+  $headers  .= "MIME-Version: 1.0\n";
+  $headers  .= "Content-type:text/plain; charset=UTF-8\n";
 
-	$technical_feedback_email_headers  .= "MIME-Version: 1.0\r\n";
-	$technical_feedback_email_headers  .= "Content-type:text/html; charset=ISO-8859-1\r\n";
-	
-		
-	mail($to, $subject, $message, $technical_feedback_email_headers);
-	
-	
-	drupal_set_message(t('Your feedback has been emailed to the UNLcms dev team. Thank you!'));
-	$form_state['redirect'] = $current_url;
+  mail($to, $subject, $message, $headers);
+
+  drupal_set_message(t('Your feedback has been emailed to the UNLcms dev team. Thank you!'));
+
   return;
 }
--- sites/all/themes/unl_wdn/theme.js
+++ sites/all/themes/unl_wdn/theme.js
@@ -1,53 +1,25 @@
 WDN.loadJQuery(function () {
-	// if there's no base tag on the page, we don't have to worry about this
-	if (WDN.jQuery('base').length == 0) {
-		return;
-	}
-	WDN.jQuery('a').click(function(e) {
-		// if this link has a hash tag
-		if (!this.href.split('#')[1]) {
-			return;
-		}
-		// and it is specifically for this page
-		if (this.getAttribute('href').split('#')[0] != '') {
-			return;
-		}
-		// and something else hasn't already customized the link
-		if (e.isDefaultPrevented()) {
-			return;
-		}
-		
-		// fix clicking the link so that it ignores the base tag
-		e.preventDefault();
-		document.location.hash = this.href.split('#')[1];
-	});
-	
-	// checking using ajax if user is logged in. then the technical feedback div is shown
-	var userLoggedIn = '';
-	
-	WDN.jQuery.ajax({
-		url: "user/unl/whoami",
-		dataType: "text",
-		success: function(data) {
-			userLoggedIn = String(data);
-			
-			if (userLoggedIn =='user_loggedin') {
-				var technicalFeedbackHtml = WDN.jQuery.ajax({
-					url: "user/unl/technical_feedback",
-					dataType: "html",
-					success: function(data) {
-						
-						var technicalFeedback = '<a id="technicalFeedbackLink">Found a bug? Report any issue with the cms (like when editing docs, uploading etc.) or give feedback</a>';
-						technicalFeedback += '<div id="technicalFeedbackForm"></div>';
-						
-						WDN.jQuery("#footer>div:nth-child(2)").append(technicalFeedback);
-						
-						WDN.jQuery("#technicalFeedbackLink").click(function() {
-							WDN.jQuery("#technicalFeedbackForm").append(data);
-						});
-					}
-				});
-			} // end of if userLoggedIn == 'user_loggedin'
-		} // end of success: function(data)
-	});
+  // if there's no base tag on the page, we don't have to worry about this
+  if (WDN.jQuery('base').length == 0) {
+    return;
+  }
+  WDN.jQuery('a').click(function(e) {
+    // if this link has a hash tag
+    if (!this.href.split('#')[1]) {
+      return;
+    }
+    // and it is specifically for this page
+    if (this.getAttribute('href').split('#')[0] != '') {
+      return;
+    }
+    // and something else hasn't already customized the link
+    if (e.isDefaultPrevented()) {
+      return;
+    }
+
+    // fix clicking the link so that it ignores the base tag
+    e.preventDefault();
+    document.location.hash = this.href.split('#')[1];
+  });
+
 });
