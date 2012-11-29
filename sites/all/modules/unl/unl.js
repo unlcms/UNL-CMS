ded44247685137189a7b8f4fc8b598d871f9e63c
Eric Rasmussen <ericrasmussen1@gmail.com>
2012-11-29 17:42:26 -0600

    [gh-546] Rewrite unl.js to use suggested Drupal jQuery pattern

--- sites/all/modules/unl/unl.js
+++ sites/all/modules/unl/unl.js
@@ -1,49 +1,50 @@
 /**
- * On IMCE pages, modify IMCE to return relative paths when a base tag is being used.
+ * @file unl.js
+ *
+ * Some basic behaviors and utility functions for Views.
  */
-jQuery('document').ready(function() {
-	// If this isn't an IMCE page, we don't need to do anything.
-	if (typeof(imce) == 'undefined') {
-		return;
-	}
-	// If we aren't using a base tag, we don't need to do anything.
-	if (!Drupal.settings.unl.use_base_tag) {
-		return;
-	}
-	// Change imce's base url to the current directory.
-	Drupal.settings.imce.furl = ".";
-	// Override imce's getURL method to remove a trailing ./ from URLs.
-	imce.realGetURL = imce.getURL;
-	imce.getURL = function(fid) {
-		var url = imce.realGetURL(fid);
-		if (url.substr(0, 2) == './') {
-			url = url.substr(2);
-		}
-		return url;
-	};
-});
+(function ($) {
 
-jQuery('document').ready(function() {
-	// if there's no base tag on the page, we don't have to worry about this
-	if (jQuery('base').length == 0) {
-		return;
-	}
-	jQuery('a').click(function(e) {
-	// if this link has a hash tag
-	if (!this.href.split('#')[1]) {
-		return;
-	}
-	// and it is specifically for this page
-	if (this.getAttribute('href').split('#')[0] != '') {
-		return;
-	}
-	// and something else hasn't already customized the link
-	if (e.isDefaultPrevented()) {
-		return;
-	}
+Drupal.behaviors.unl = {
+  attach: function (context, settings) {
+    if (!Drupal.settings.unl.use_base_tag) {
+      return;
+    }
 
-	// fix clicking the link so that it ignores the base tag
-	e.preventDefault();
-	document.location.hash = this.href.split('#')[1];
-	});
-});
+    // Modify IMCE to return relative paths when a base tag is present.
+    if (typeof(imce) !== 'undefined') {
+      // Change IMCE base url to the current directory.
+      Drupal.settings.imce.furl = ".";
+      // Override IMCE getURL method to remove a trailing ./ from URLs.
+      imce.realGetURL = imce.getURL;
+      imce.getURL = function(fid) {
+        var url = imce.realGetURL(fid);
+        if (url.substr(0, 2) == './') {
+          url = url.substr(2);
+        }
+        return url;
+      };
+    }
+
+    // Make links ignore the base tag.
+    $('a').click(function(e) {
+      // If this link has a hash tag,
+      if (!this.href.split('#')[1]) {
+        return;
+      }
+     // and it is specifically for this page,
+      if (this.getAttribute('href').split('#')[0] != '') {
+        return;
+      }
+      // and something else hasn't already customized the link,
+      if (e.isDefaultPrevented()) {
+        return;
+      }
+
+      e.preventDefault();
+      document.location.hash = this.href.split('#')[1];
+    });
+  }
+};
+
+})(jQuery);
