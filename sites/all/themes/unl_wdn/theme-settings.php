c5c29c310eadc77e6ded7a9071c609025496198f
Eric Rasmussen <ericrasmussen1@gmail.com>
2013-02-07 17:15:50 -0600

    [gh-592] Typo

--- sites/all/themes/unl_wdn/theme-settings.php
+++ sites/all/themes/unl_wdn/theme-settings.php
@@ -117,7 +117,7 @@ function unl_wdn_form_system_theme_settings_alter(&$form, &$form_state) {
       '#type' => 'checkbox',
       '#title' => t('Disable drill-down menus'),
       '#default_value' => theme_get_setting('drill_down'),
-      '#description' => t('Turns off changing the navigation if you 2+ levels deep with deeper enabled menu links.'),
+      '#description' => t('Turns off changing the navigation if you are 2+ levels deep with even deeper enabled menu links.'),
     ),
     'zen_forms' => array(
       '#type' => 'checkbox',
