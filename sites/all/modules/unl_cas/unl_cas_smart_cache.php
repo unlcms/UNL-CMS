edd127fd6a5ab3fd5cd52ffc037cf4937e03e529
Eric Rasmussen <ericrasmussen1@gmail.com>
2017-01-30 15:01:55 -0600

    Update comments

--- sites/all/modules/unl_cas/unl_cas_smart_cache.php
+++ sites/all/modules/unl_cas/unl_cas_smart_cache.php
@@ -11,18 +11,17 @@ function unl_cas_smart_cache_force_varnish_for_user($username) {
   $account = user_load_by_name($username);
   
   if (!$account) {
-    //Couldn't find the user, so don't use smart cache.
+    // Couldn't find the user, so don't use smart cache.
     return false;
   }
 
   if (count($account->roles) > 1) {
-    //The user has more than one role, which means that they are more than just a guest
+    // The user has more than one role, which means that they are more than just a guest.
     return false;
   }
   
   //TODO: check if the user has any edit access to specific nodes (sounds like this might be possible)
-  
-  //else there is only one role left... make sure it is the authenticated user role
+
   return true;
 }
 
@@ -37,7 +36,7 @@ function unl_cas_smart_cache_force_varnish_for_site() {
   }
   
   if (module_exists('unl_access')) {
-    //Turn off smart cache because unl_access is enabled... We could improve this so only specific resources protected by unl_access are not cached.
+    // Turn off smart cache because unl_access is enabled... We could improve this so only specific resources protected by unl_access are not cached.
     return false;
   }
   
@@ -52,18 +51,19 @@ function unl_cas_smart_cache_force_varnish_for_site() {
 function unl_cas_smart_cache_run_for_user($username) {
   //do we really need to log the user in? Should we set a cookie to have VARNISH ignore the logged in state?
   if (unl_cas_smart_cache_force_varnish_for_user($username) && unl_cas_smart_cache_force_varnish_for_site()) {
-    //set a cookie to tell varnish to always run
+    // Set a cookie to tell varnish to always run
     setcookie('unlcms_force_varnish', 'true', 0, base_path());
 
-    //Redirect back
+    // Redirect back.
     $destination = drupal_get_destination();
     unset($_GET['destination']);
     drupal_goto($destination['destination']);
 
-    //don't proceed.
+    // Don't proceed.
     return;
-  } else {
-    //make sure the force_varnish cookie is turned off (delete the cookie
+  }
+  else {
+    // Make sure the force varnish cookie is turned off. (Delete the cookie.)
     setcookie('unlcms_force_varnish', 'false', time() - 3600, base_path());
   }
 }
