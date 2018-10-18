3160b4a2f0c5a2e57374b1341a8d3d778aba2923
Eric Rasmussen <ericrasmussen1@gmail.com>
2018-10-18 10:36:16 -0500

    Update scheduler 7.x-1.5

--- sites/all/modules/scheduler/scheduler_vertical_tabs.js
+++ sites/all/modules/scheduler/scheduler_vertical_tabs.js
@@ -1,3 +1,7 @@
+/**
+ * @file
+ * jQuery to provide summary information inside vertical tabs.
+ */
 
 (function ($) {
 
@@ -6,8 +10,17 @@
  */
 Drupal.behaviors.scheduler_settings = {
   attach: function (context) {
-	// Provide summary when editting a node.
-	$('fieldset#edit-scheduler-settings', context).drupalSetSummary(function(context) {
+
+    // Add the theme name as an additional class to the vertical-tabs div. This
+    // was originally used in scheduler.css to rectify the style for collapsible
+    // fieldsets. Left here in case any custom themes are still using it.
+    // See https://www.drupal.org/node/1172040 and
+    // https://www.drupal.org/node/2715479
+    var theme = Drupal.settings.ajaxPageState['theme'];
+    $('div.vertical-tabs').addClass(theme);
+
+    // Provide summary when editing a node.
+    $('fieldset#edit-scheduler-settings', context).drupalSetSummary(function(context) {
       var vals = [];
       if ($('#edit-publish-on').val() || $('#edit-publish-on-datepicker-popup-0').val()) {
         vals.push(Drupal.t('Scheduled for publishing'));
@@ -32,7 +45,6 @@ Drupal.behaviors.scheduler_settings = {
       }
       return vals.join('<br/>');
     });
-
   }
 };
 
