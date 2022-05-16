12656cfc36289c45caddc4bc36155862a2799fc5
Eric Rasmussen <ericrasmussen1@gmail.com>
2022-05-16 15:32:20 -0500

    Cas change Closes #955 (#964)
    * Add cas module
    
    * Initial effort to modify unl_cas to primarily rely on cas module
    
    * Add cas settings to settings example file
    
    * Update sample cas settings host name
    
    * Update unl_url_get_contents() to remove Zend; Change logout callback to run cas_logout()
    
    * Remove ZendFramework and NmcFramework
    
    * Fix the (& ) backets in the LDAP search
    
    * Need to run unl_cas_import_user() on login; It was previoulsy in unl_cas_validate_ticket() that was removed with the move to the cas module
    
    * Update unl_five submodule pointer
    
    * Limit access to some of the cas module items
    
    * Add ZendFramework back in so a PR diff is readable

--- sites/all/modules/unl/includes/common.php
+++ sites/all/modules/unl/includes/common.php
@@ -1,28 +1,14 @@
 <?php
 
-function unl_load_zend_framework() {
-  static $isLoaded = FALSE;
-
-  if ($isLoaded) {
-    return;
-  }
-
-  set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/../../../libraries');
-  require_once 'Zend/Loader/Autoloader.php';
-  $autoloader = Zend_Loader_Autoloader::getInstance();
-  $autoloader->registerNamespace('Unl_');
-  $isLoaded = TRUE;
-}
-
 /**
  * Custom function to get the db settings for the 'main' site.
  * @return array
  */
 function unl_get_shared_db_settings() {
   if (file_exists(DRUPAL_ROOT . '/sites/all/settings.php')) {
-    require DRUPAL_ROOT . '/sites/all/settings.php';
+    @require DRUPAL_ROOT . '/sites/all/settings.php';
   }
-  require DRUPAL_ROOT . '/sites/default/settings.php';
+  @require DRUPAL_ROOT . '/sites/default/settings.php';
 
   return $databases;
 }
@@ -201,10 +187,8 @@ function unl_user_is_administrator() {
  * @param string $url
  * @param resource $context
  */
-function unl_url_get_contents($url, $context = NULL, &$headers = array())
-{
-  unl_load_zend_framework();
-  if (!Zend_Uri::check($url)) {
+function unl_url_get_contents($url, $context = NULL, &$headers = array()) {
+  if (!valid_url($url)) {
     watchdog('unl', 'A non-url was passed to %func().', array('%func' => __FUNCTION__), WATCHDOG_WARNING);
     return FALSE;
   }
